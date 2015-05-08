71ccb29d3faf203c64735ca9635637a5ad4f6807
liv3dn8as <twcoplien@gmail.com>
2015-05-08 13:54:00 -0500

    fixing stuff

--- vs985.mk
+++ vs985.mk
@@ -17,12 +17,13 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from vs985 device
-$(call inherit-product, device/lge/vs985/device.mk)
-
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := vs985
 PRODUCT_NAME := full_vs985
-PRODUCT_BRAND := lge
+PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-VS985
 PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Inherit from vs985 device
+$(call inherit-product, device/lge/vs985/device.mk)
